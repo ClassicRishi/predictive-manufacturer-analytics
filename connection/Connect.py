6c30b23501348e0f2f61c54d04fd68c7d8e5f7c5
ClassicRishi <rishi.kaveri037@gmail.com>
2026-04-27 09:18:07 +0530

    secrets-updated

--- connection/Connect.py
+++ connection/Connect.py
@@ -4,7 +4,7 @@ import streamlit as stl
 class MongoConnectDriver:
     def __init__(self) -> None:
         self.client = mongo.MongoClient(stl.secrets.get("MONGODB_URI"))
-        self.db = self.client['manufacturers']
-        self.collection = self.db['records']
+        self.db = self.client[stl.secrets.get("DB")]
+        self.collection = self.db[stl.secrets.get("COLLECTION")]
     def connect(self):
         return self.collection
